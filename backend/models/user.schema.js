6af5db54304f53a98800a682631c83f183904251
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-07 11:47:31 +0530

    added methods and hooks in userModel

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from 'bcryptjs';
+import  Jwt  from "jsonwebtoken";
+import crypto from 'crypto';
+
+// custom files import
+import config  from "../config/env";
 import authRoles from "../utils/authRoles";
 
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name:{
         type:String,
-        require:[true,'Name is required'],
+        required:[true,'Name is required'],
         maxLength:[40, 'Name should be less then 40 Characters']
     },
     email:{
         type:String,
-        require:[true, 'Email is required'],
+        required:[true, 'Email is required'],
         unique:true,
-        validate:validator.isEmail
+        validate:[validator.isEmail, 'Please enter email in correct format']
     },
     password:{
         type:String,
@@ -25,15 +31,53 @@ const userSchema = mongoose.Schema({
         enum:Object.keys(authRoles),
         default:authRoles.USER
     },
+    photo:{
+        id:{
+            type: String,
+            required:true
+        },
+        secure_url:{
+            type: String,
+            required:true
+        }
+    },
     forgotPasswordToken:String,
     forgotPasswordExpiry:Date,
+},
+{timestamps:true}
+);
+
+// encrypt password before save-HOOKS
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){return next();}
+    this.password = await bcrypt.hash(this.password, 10);
+    next()
 })
 
+// validate the password with passed on user password
+userSchema.methods.isValidatePassword = async function(userEnteredPassword){
+  return await bcrypt.compare(userEnteredPassword, this.password)
+}
 
-// methods
-userSchema.method.get
+// create and return JwtToken
+userSchema.methods.getJwtToken = function() {
+   return Jwt.sign({id:this._id}, config.JWT_SECRET,{expiresIn: config.JWT_EXPIRTY});
+}
 
 
+// generate forgot password token(String format)
+userSchema.methods.getForgotPasswordToken = function(){
+   const forgotToken = crypto.randomBytes(24).toString('hex');
+
+   // getting a hash - make sure to get a hash on backend
+   this.forgotPasswordToken = crypto.createHash('sha256').update(forgotToken).digest('hex');
+
+   // time of token expiery
+   this.forgotPasswordExpiry = Date.now() + 20*60*1000;
+
+   return forgotToken;
+}
 
 
+// export user Model
 module.exports = mongoose.model('User',userSchema);
