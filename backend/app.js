bd94fe1dd56fde860f721ab769659633c3c0466b
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-07 11:18:37 +0530

    middleware added to app

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const home =  require('./routes/home');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 const app = express();
+const home =  require('./routes/home');
 
-// m middleware
+// morgan middleware
+app.use(morgan('tiny'))
+
+// regular middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+// cookies and file middleware
+app.use(cookieParser())
+app.use(fileUpload())
 
 
 // import all routes here
@@ -14,5 +23,5 @@ app.use('/api/v1', home)
 
 
 
-
+// export express app
 module.exports= app;
