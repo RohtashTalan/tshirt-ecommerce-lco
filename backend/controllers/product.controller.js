8a014d9ca38171ec3906e76cf5a3970776bc829f
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-08 16:57:56 +0530

    add product controller added

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,38 @@
+const Product = require('../models/product.schema')
 const BigPromise = require("../middlewares/bigPromise");
 const CustomError = require("../utils/customError");
-const cookieToken = require("../utils/cookieToken");
-const mailHelper = require("../utils/emailHelper");
 const crypto = require("crypto");
+const cloudinary = require("cloudinary").v2;
+
+
+
+exports.addProduct= BigPromise(async(req, res, next) => {
+
+    let imageArray = []
+    if(!req.files){return next(new CustomError('Images are required', 401))}
+
+    if(req.files){
+        for (let i = 0; i < req.files.photos.length; i++) {
+            let result = await cloudinary.uploader.upload(req.files.photos[i].tempFilePath, {
+                folder: 'products'
+            });
+            imageArray.push({
+                    id: result.public_id,
+                    secure_url: result.secure_url
+                })
+        }
+    }
+
+    req.body.photos = imageArray;
+    req.body.user = req.user.id;
+
+    const product = await Product.create(req.body);
+
+    res.status(200).json({
+        success: true,
+        message: 'product added successfully',
+        product,
+        entered: req.body
+    })
+
+})
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {isLoggedIn, customRole} = require('../middlewares/user');
-const {product} = require('../controllers/product.controller');
+const {addProduct} = require('../controllers/product.controller');
 
 
-
+router.route('/addproduct').post(isLoggedIn, addProduct);
 
 module.exports = router;
